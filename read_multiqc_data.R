86377ceb290f3d33ccd41e5c5e5fcec6c7b8a25d
Royberts <rroberts3@islander.tamucc.edu>
2022-04-26 17:19:19 -0500

    script updates

--- read_multiqc_data.R
+++ read_multiqc_data.R
@@ -1,5 +1,5 @@
 # Package names
-packages <- c("tidyverse", "lubridate", "janitor", "tidyr", "purrr", "furrr")
+packages <- c("tidyverse", "janitor")
 
 # Install packages not yet installed
 installed_packages <- packages %in% rownames(installed.packages())
@@ -10,15 +10,19 @@ if (any(installed_packages == FALSE)) {
 # Packages loading
 invisible(lapply(packages, library, character.only = TRUE))
 
-read_multiqc_fastqc <- function(PATH_MQC_FQC = "../preprocessing_data_ssl/multiqc_fastqc/all_treatments_report_data/",
+#### Functions ####
+
+read_multiqc_fastqc <- function(PATH_MQC_FQC,
                                 FILE1 = "multiqc_general_stats.txt",
-                                FILE2 = "multiqc_fastqc.txt"){
+                                FILE2 = "multiqc_fastqc.txt",
+                                FILE3 = "mqc_fastqc_overrepresented_sequencesi_plot_1.txt"){
   read_tsv(paste(PATH_MQC_FQC, FILE1, sep = "")) %>%
     inner_join(read_tsv(paste(PATH_MQC_FQC, FILE2, sep = ""))) %>%
+    inner_join(read_tsv(paste(PATH_MQC_FQC, FILE3, sep = ""))) %>%
     clean_names()
 }
 
-read_multiqc_fastp <- function(PATH_MQC_FP = "../preprocessing_data_ssl/multiqc_fastp/multiqc_data/",
+read_multiqc_fastp <- function(PATH_MQC_FP,
                                FILE1 = "multiqc_general_stats.txt",
                                FILE2 = "mqc_fastp_filtered_reads_plot_1.txt"){
   read_tsv(paste(PATH_MQC_FP, FILE1, sep = "")) %>%
@@ -26,7 +30,7 @@ read_multiqc_fastp <- function(PATH_MQC_FP = "../preprocessing_data_ssl/multiqc_
     clean_names()
 }
 
-read_multiqc_fastqscreen <- function(PATH_MQC_FQS = "../preprocessing_data_ssl/multiqc_fastq_screen/multiqc_data/",
+read_multiqc_fastqscreen <- function(PATH_MQC_FQS,
                                      FILE = "multiqc_fastq_screen.txt"){
   read_tsv(paste(PATH_MQC_FQS, FILE, sep = "")) %>%
     clean_names()
